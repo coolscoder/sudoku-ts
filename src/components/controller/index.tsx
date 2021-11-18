64af0b102704c4109135216c90c0eb0fdaa64795
Koichi Noda <koichi.p.noda@gmail.com>
2021-11-19 02:41:18 +0900

    feat: clean up code bases

--- src/components/controller/index.tsx
+++ src/components/controller/index.tsx
@@ -1,4 +1,4 @@
-import React, { FC, useCallback, useEffect, useState } from 'react';
+import React, { FC, useEffect, useState } from 'react';
 import { useDispatch } from 'react-redux';
 import { AnyAction, Dispatch } from 'redux';
 import Select from 'react-select'
