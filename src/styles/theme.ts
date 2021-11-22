39df15821662f4e48c53327786d0fbd2c8b79ed3
Koichi Noda <koichi.p.noda@gmail.com>
2021-11-22 22:47:20 +0900

    feat: remove unnecessary code bases

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -1,9 +1,5 @@
-// eslint-disable-next-line
 export default {
 	colors: {
-		// background: 'radial-gradient(#282c34cc, #282c34)',
-		// background: '#FFEFD5',
-		// black: '#282c34',
 		blue: '#a0e9fd',
 		white: 'white',
 		background: 'radial-gradient(#a0e9fd, #16697a)',
